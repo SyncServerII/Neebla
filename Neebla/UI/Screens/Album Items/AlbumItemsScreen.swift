ff9b519bba73ab5cad464fa1fa34416bb7012452
Christopher Prince <chris@SpasticMuffin.biz>
2020-12-01 20:47:11 -0700

    Fixed animation problem and grid spacing issue.

--- Neebla/UI/Screens/Album Items/AlbumItemsScreen.swift
+++ Neebla/UI/Screens/Album Items/AlbumItemsScreen.swift
@@ -6,34 +6,49 @@ import CustomModalView
 
 struct AlbumItemsScreen: View {
     @ObservedObject var viewModel:AlbumItemsViewModel
-    let gridItemLayout = [GridItem(.adaptive(minimum: 50), spacing: 20)]
-    @State var object: ServerObjectModel?
-    @State var enableNavLink: Bool = false
     
-    var body: some View {        
-        RefreshableScrollView(refreshing: $viewModel.loading) {
-            LazyVGrid(columns: gridItemLayout) {
-                ForEach(viewModel.objects, id: \.fileGroupUUID) { item in
-                    AlbumItemsScreenCell(object: item)
-                        .onTapGesture {
-                            object = item
-                            viewModel.showCellDetails = true
-                        }
-
-                    // Without this conditional, "spacer" cells show up in the grid.
-                    if viewModel.showCellDetails, let object = object {
-                        // The `NavigationLink` works here because the `MenuNavBar` contains a `NavigationView`.
-                        NavigationLink(
-                            destination:
-                                // If I just use `item` directly in this-- oddly, it doesn't reference the same object as for `AlbumItemsScreenCell` above.
-                                ObjectDetailsView(object: object),
-                            isActive:
-                                $viewModel.showCellDetails) {
-                        }
-                    } // end if
-                } // end ForEach
-            } // end LazyVGrid
-            .padding(10)
+    /* It seems hard to get the spacing to work out reasonably. At first, it looked OK on iPhone 11 but not on iPhone 8-- on iPhone 8 there was no spacing. In this case I was using:
+    
+        let gridItemLayout = [GridItem(.adaptive(minimum: 50), spacing: 20)]
+      
+      Just changing the `spacing` doesn't really help. What seems to be going on is that the LazyVGrid is trying to fit as many cells as it can per row. And if that means no spacing, then there is no spacing.
+      
+      What's helping is to use the image dimension as the minimum. This is looking OK on iPhone 8 and iPhone 11.
+    */
+    let gridItemLayout = [
+        GridItem(.adaptive(minimum: GenericImageIcon.dimension), spacing: 5)
+    ]
+    
+    @State var object: ServerObjectModel?
+    
+    var body: some View {
+        VStack {
+            RefreshableScrollView(refreshing: $viewModel.loading) {
+                LazyVGrid(columns: gridItemLayout) {
+                    ForEach(viewModel.objects, id: \.fileGroupUUID) { item in
+                        AlbumItemsScreenCell(object: item)
+                            .onTapGesture {
+                                object = item
+                                viewModel.showCellDetails = true
+                            }
+                    } // end ForEach
+                } // end LazyVGrid
+            }.padding(5)
+            
+            // Had a problem with return animation for a while: https://stackoverflow.com/questions/65101561
+            // The solution was to take the NavigationLink out of the scrollview/LazyVGrid above.
+            if let object = object {
+                // The `NavigationLink` works here because the `MenuNavBar` contains a `NavigationView`.
+                NavigationLink(
+                    destination:
+                        ObjectDetailsView(object: object),
+                    isActive:
+                        $viewModel.showCellDetails) {
+                    EmptyView()
+                }
+                .frame(width: 0, height: 0)
+                .disabled(true)
+            } // end if
         }
         .alert(isPresented: $viewModel.presentAlert, content: {
             let message:String = viewModel.alertMessage
