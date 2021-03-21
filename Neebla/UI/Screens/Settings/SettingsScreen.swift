85800e150f3e14f3d7b7bcff4f1d0d84a9f6e8f1
Christopher Prince <chris@SpasticMuffin.biz>
2021-03-21 16:59:19 -0600

    Improved About text. Added scroll view to Settings.

--- Neebla/UI/Screens/Settings/SettingsScreen.swift
+++ Neebla/UI/Screens/Settings/SettingsScreen.swift
@@ -21,81 +21,84 @@ struct SettingsScreenBody: View {
     @StateObject var alerty = AlertySubscriber(publisher: Services.session.userEvents)
     
     var body: some View {
-        VStack(spacing: 40) {
-            Spacer().frame(height: 20)
-            
-            VStack {
-                Text("User Name")
-                    .bold()
-                    
-                TextField("User name", text: $settingsModel.userName ?? "")
-                    .multilineTextAlignment(.center)
-                    .padding(5)
-                    .border(colorScheme == .dark ? Color.black : Color(UIColor.lightGray))
-                    .frame(
-                        // I tried using a GeometryReader here to make this width a function of screen width, but that breaks the center alignment of the VStack. Grrrr.
-                        width: textFieldWidth
-                    )
-                    // Background color of TextField is fine in non-dark mode. But in dark mode, by default the user can't see the outline of the text field-- and I like them to be able to do that.
-                    .if(colorScheme == .dark) {
-                        $0.background(Color(UIColor.darkGray))
-                    }
-                    
-                HStack {
-                    Button(action: {
-                        settingsModel.updateUserNameOnServer(userName: settingsModel.userName) { success in
-                            if success {
-                                hideKeyboard()
-                            }
+        // Using a scroll view because when you rotate to landscape on iPhone, it looks bad without it. Too scrunched up.
+        ScrollView {
+            VStack(spacing: 40) {
+                Spacer().frame(height: 20)
+                
+                VStack {
+                    Text("User Name")
+                        .bold()
+                        
+                    TextField("User name", text: $settingsModel.userName ?? "")
+                        .multilineTextAlignment(.center)
+                        .padding(5)
+                        .border(colorScheme == .dark ? Color.black : Color(UIColor.lightGray))
+                        .frame(
+                            // I tried using a GeometryReader here to make this width a function of screen width, but that breaks the center alignment of the VStack. Grrrr.
+                            width: textFieldWidth
+                        )
+                        // Background color of TextField is fine in non-dark mode. But in dark mode, by default the user can't see the outline of the text field-- and I like them to be able to do that.
+                        .if(colorScheme == .dark) {
+                            $0.background(Color(UIColor.darkGray))
                         }
-                    }, label: {
-                        Text("Update")
-                    })
-                    .enabled(settingsModel.userNameChangeIsValid)
+                        
+                    HStack {
+                        Button(action: {
+                            settingsModel.updateUserNameOnServer(userName: settingsModel.userName) { success in
+                                if success {
+                                    hideKeyboard()
+                                }
+                            }
+                        }, label: {
+                            Text("Update")
+                        })
+                        .enabled(settingsModel.userNameChangeIsValid)
 
-                    Button(action: {
-                        settingsModel.userName = settingsModel.initialUserName
-                    }, label: {
-                        Text("(Reset)")
-                    })
-                    .isHiddenRemove(settingsModel.userName == settingsModel.initialUserName)
+                        Button(action: {
+                            settingsModel.userName = settingsModel.initialUserName
+                        }, label: {
+                            Text("(Reset)")
+                        })
+                        .isHiddenRemove(settingsModel.userName == settingsModel.initialUserName)
+                    }
                 }
-            }
-            
-            Button(action: {
-                settingsModel.sheet = .albumList
-            }, label: {
-                Text("Remove yourself from an album")
-            })
-            
-            Button(action: {
-                let action = {
-                    settingsModel.sheet = .emailDeveloper(addAttachments: settingsModel)
+                
+                Button(action: {
+                    settingsModel.sheet = .albumList
+                }, label: {
+                    Text("Remove yourself from an album")
+                })
+                
+                Button(action: {
+                    let action = {
+                        settingsModel.sheet = .emailDeveloper(addAttachments: settingsModel)
+                    }
+                    let cancelAction = {
+                        settingsModel.sheet = .emailDeveloper(addAttachments: nil)
+                    }
+                    showAlert(AlertyHelper.customAction(title: "Send logs?", message: "Would you like to send Neebla's logs to the developer?", actionButtonTitle: "Yes", action: action, cancelTitle: "No", cancelAction: cancelAction))
+                }, label: {
+                    Text("Contact developer")
+                })
+                
+                Spacer()
+                
+                Button(action: {
+                    settingsModel.sheet = .aboutApp
+                }, label: {
+                    Text("About")
+                })
+                
+                VStack {
+                    Text("Version/Build")
+                        .bold()
+                    Text(settingsModel.versionAndBuild)
                 }
-                let cancelAction = {
-                    settingsModel.sheet = .emailDeveloper(addAttachments: nil)
-                }
-                showAlert(AlertyHelper.customAction(title: "Send logs?", message: "Would you like to send Neebla's logs to the developer?", actionButtonTitle: "Yes", action: action, cancelTitle: "No", cancelAction: cancelAction))
-            }, label: {
-                Text("Contact developer")
-            })
-            
-            Spacer()
-            
-            Button(action: {
-                settingsModel.sheet = .aboutApp
-            }, label: {
-                Text("About")
-            })
-            
-            VStack {
-                Text("Version/Build")
-                    .bold()
-                Text(settingsModel.versionAndBuild)
-            }
-            
-            Spacer().frame(height: 20)
-        } // end VStack
+                
+                Spacer().frame(height: 20)
+            } // end VStack
+        } // end ScrollView
         .alertyDisplayer(show: $alerty.show, subscriber: alerty)
         .sheetyDisplayer(item: $settingsModel.sheet, subscriber: alerty) { sheet in
             switch sheet {
